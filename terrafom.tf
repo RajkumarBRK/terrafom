93804836d95a29875964959a0db69f0df76ca954
RajkumarBRK <80801976+RajkumarBRK@users.noreply.github.com>
2022-01-29 13:17:21 +0530

    Update terrafom.tf

--- terrafom.tf
+++ terrafom.tf
@@ -4,7 +4,7 @@ access_key="AKIA35APZZ3RFXZVG5LV"
 secret_key="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"
 }
 resource "aws_instance" "abc" {
-ami="ami-0ed9277fb7eb570c9"
+ami="ami-0a8b4cd432b1c3063"
 instance_type="t2.micro"
 availability_zone="us-east-1a"
 key_name="nvr"
